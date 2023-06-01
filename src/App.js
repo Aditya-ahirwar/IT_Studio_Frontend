b7f04678f8ec7772f75cc9ff5ee6e1f57c139db0
aditya_ahirwar <adityaahirwar3@gmail.com>
2023-06-02 01:35:00 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,177 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import Form from './components/Form';
+import Update from './components/Update'
 
-function App() {
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin-bottom : 10px;
+`;
+
+const TableRow = styled.tr`
+border : 1px solid black;
+  &:nth-child(even) {
+    background-color: #f0f0f0;
+  }
+`;
+
+const TableHeader = styled.th`
+  padding: 10px;
+  text-align: left;
+  border : 2px solid black;
+`;
+
+const TableCell = styled.td`
+  padding: 10px;
+  border : 2px solid black;
+`;
+
+const Button = styled.button`
+  margin-left: 10px;
+  background-color : blue;
+  color : white;
+  border : none;
+  cursor : pointer;
+  border-radius : 2px;
+  font-size : 15px;
+  padding : 5px;
+`;
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [updatePopup, setupdatePopup] = useState(false);
+  const [currRow, setCurrRow] = useState({})
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    axios.get('http://localhost:5000/')
+      .then((res) => { 
+        setData(res.data);
+        console.log(data)
+
+      }).catch((err) => { console.log(err)})
+  };
+
+  const handleCheckboxChange = (event, row) => {
+    if (event.target.checked) {
+      setSelectedRows([...selectedRows, row]);
+    } else {
+      setSelectedRows(selectedRows.filter((currRow) => currRow._id !== row._id));
+    }
+  };
+
+  const handleDeleteRow = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/${id}`); // Replace with your API endpoint
+      fetchData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUpdateRow = async (row) =>{
+    setCurrRow(row)
+    setupdatePopup(true)
+  }
+
+  const handleUpdateData =  async (formData) => {
+    try {
+      await axios.put(`http://localhost:5000/${currRow._id}`, formData);
+      fetchData();
+    } catch (error) {
+      console.error(error);
+    }
+    // console.log(formData)
+  };
+
+  const handleCloseUpdate = ()=>{
+    setupdatePopup(false)
+  }
+
+
+  const handleOpenPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  const handleSaveData = async (formData) => {
+    try {
+      await axios.post(`http://localhost:5000/add`, formData);
+      fetchData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const sendEmail = async () =>{
+    console.log(selectedRows)
+    try {
+      await axios.post(`http://localhost:5000/send`, selectedRows);
+    } catch (error) {
+      console.error(error);
+    }
+
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Table>
+        <thead>
+          <TableRow>
+            <TableHeader>Select</TableHeader>
+            <TableHeader>Name</TableHeader>
+            <TableHeader>Phone</TableHeader>
+            <TableHeader>Email</TableHeader>
+            <TableHeader>Hobby</TableHeader>
+            <TableHeader>Actions</TableHeader>
+          </TableRow>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <TableRow key={row._id}>
+              <TableCell>
+                <input
+                  type="checkbox"
+                  checked={selectedRows.includes(row)}
+                  onChange={(event) => handleCheckboxChange(event, row)}
+                />
+              </TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.phone}</TableCell>
+              <TableCell>{row.email}</TableCell>
+              <TableCell>{row.hobby}</TableCell>
+              <TableCell style={{display : "flex", gap : "5px"}}>
+                <Button onClick={() => handleDeleteRow(row._id)}>Delete</Button>
+                <Button onClick={() => handleUpdateRow(row)}>Update</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </tbody>
+      </Table>
+      <Button onClick={handleOpenPopup}>Add Row</Button>
+      <Button onClick={sendEmail}>Send</Button>
+      <Form isOpen={isPopupOpen} onClose={handleClosePopup} onSave={handleSaveData} />
+      <Update isOpen={updatePopup} onClose={handleCloseUpdate} onSave={handleUpdateData} currRow = {currRow}/>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/Update.jsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  width : 250px;
+  padding : 50px;
+  height : 400px;
+`;
+
+const FormTitle = styled.h2`
+  margin-top: 0;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 200px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  margin-left: 10px;
+  background-color : blue;
+  color : white;
+  border : none;
+  cursor : pointer;
+  border-radius : 2px;
+  font-size : 15px;
+  padding : 5px;
+`;
+
+const Form = ({ isOpen, onClose, onSave, currRow }) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [hobby, setHobby] = useState('');
+  // console.log(currRow);
+
+  useEffect(()=>{
+    setName(currRow.name)
+    setPhone(currRow.phone)
+    setEmail(currRow.email)
+    setHobby(currRow.hobby)
+    // console.log(name)
+    // console.log(phone)
+    // console.log(email)
+    // console.log(hobby)
+
+  }, [currRow])
+  const handleSave = () => {
+
+    // Call the save function passed as a prop
+    onSave({
+      name : name,
+      phone : phone,
+      email : email,
+      hobby : hobby
+    });
+
+    // Clear the form inputs
+    setName('');
+    setPhone('');
+    setEmail('');
+    setHobby('');
+
+    // Close the form
+    onClose();
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <FormContainer>
+          <FormTitle>Update Row</FormTitle>
+          <FormGroup>
+            <Label>Name:</Label>
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Phone Number:</Label>
+            <Input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Email:</Label>
+            <Input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Hobby:</Label>
+            <Input
+              type="text"
+              value={hobby}
+              onChange={(e) => setHobby(e.target.value)}
+            />
+          </FormGroup>
+          <ButtonContainer>
+            <Button onClick={handleSave}>Save</Button>
+            <Button onClick={onClose} style={{backgroundColor : "red"}}>Cancel</Button>
+          </ButtonContainer>
+        </FormContainer>
+      )}
+    </>
+  );
+};
+
+export default Form;
